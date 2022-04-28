48598ec9d457fba1f3dc8d4153595a0371889dc6
ddophi98 <lightcloud98@gmail.com>
2022-04-28 09:52:03 +0900

    프로젝트 생성

---
+++ FormatDescription/FormatDescriptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormatDescriptionApp.swift
+//  FormatDescription
+//
+//  Created by 김동락 on 2022/04/28.
+//
+
+import SwiftUI
+
+@main
+struct FormatDescriptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
